ae33fe7c7d1cd5f7a659e13f85df2b58154ab5aa
navyCK <ckck81026@gmail.com>
2020-12-09 15:47:21 +0900

    결과 버튼 터치 시 다이얼로그 창 표시되며 키, 몸무게, BMI 표시

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -21,13 +23,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var _text = 'Hello';
+  final _heightTextEditController = TextEditingController();
+  final _weightTextEditController = TextEditingController();
+  int height, weight;
+
+  @override
+  void dispose() {
+    _heightTextEditController.dispose();
+    _weightTextEditController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello World'),
+        title: Text('비만도 계산기'),
       ),
       body: Column(
         children: <Widget>[
@@ -35,30 +46,62 @@ class _MyHomePageState extends State<MyHomePage> {
             height: 20,
           ),
           TextField(
+            controller: _heightTextEditController,
             decoration: InputDecoration(
               border: OutlineInputBorder(),
               labelText: '키를 입력하세요',
             ),
+            onChanged: (text) {
+              print(text);
+            },
           ),
           SizedBox(
             height: 20,
           ),
           TextField(
+            controller: _weightTextEditController,
             decoration: InputDecoration(
               border: OutlineInputBorder(),
               labelText: '몸무게를 입력하세요',
             ),
+            onChanged: (text) {
+              print(text);
+            },
           ),
           SizedBox(
             height: 20,
           ),
           RaisedButton(
             child: Text('결과'),
-            color: Colors.orange,
+            color: Colors.blue,
+            textColor: Colors.white,
             onPressed: () {
+              print(_heightTextEditController.text);
+              print(_weightTextEditController.text);
 
+              height = int.parse(_heightTextEditController.text);
+              weight = int.parse(_weightTextEditController.text);
+
+
+              return showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text(
+                          '키 : ' + _heightTextEditController.text +
+                          '\n몸무게 : '  + _weightTextEditController.text +
+                          '\nBMI : ' + (weight / pow(height / 100, 2)).toStringAsFixed(2).toString()
+
+                      ),
+                    );
+                  });
             },
           ),
+          SizedBox(
+            height: 20,
+          ),
+
+
 
         ],
       )
